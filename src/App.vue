d70be137e14e9e394bfd968a487338db8326ea96
yocosaka <sakayoco55@gmail.com>
2021-09-06 15:01:29 +0900

    Add Block component and data isPlaying and delay to handle the game and passed them as props to Block

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="startGame" :disabled="isPlaying">Play</button>
+  <Block :delay="delay" v-if="isPlaying" />
 </template>
 
 <script>
+import Block from './components/Block.vue'
+
 export default {
   name: 'App',
-  components: {  }
+  components: { Block },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    startGame(){
+      this.delay = 2000 + Math.random() * 5000 // 2000 - 5000
+      this.isPlaying = true
+      // console.log(this.delay)
+    }
+  }
 }
 </script>
 
