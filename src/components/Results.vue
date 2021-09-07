9c42700ed5e3cf2ca39ca41e5000f063ecca98c9
yocosaka <sakayoco55@gmail.com>
2021-09-07 14:16:46 +0900

    Add rank to Results.vue component

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,32 @@
 <template>
   <p>ReactionTime: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-  props: ['score']
+  props: ['score'],
+  data(){
+    return {
+      rank: null
+    }
+  },
+  mounted(){
+    if(this.score < 460){
+      this.rank = 'Fantastic Ninja!'
+    } else if(this.score < 1000){
+      this.rank = 'Normal Ninja'
+    } else {
+      this.rank = 'Train harder!'
+    }
+  }
 }
 </script>
 
 <style>
-
+.rank{
+  font-size: 1.4em;
+  color: salmon;
+  font-weight: bold;
+}
 </style>
