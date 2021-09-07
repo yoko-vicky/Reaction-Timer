3c82fb26896fc951812ef71402a56e4f69c4d759
yocosaka <sakayoco55@gmail.com>
2021-09-07 14:18:31 +0900

    Add style for button

--- src/App.vue
+++ src/App.vue
@@ -36,12 +36,27 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #444;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+  }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -26,7 +26,7 @@ export default {
 <style>
 .rank{
   font-size: 1.4em;
-  color: salmon;
+  color: #0faf87;
   font-weight: bold;
 }
 </style>
